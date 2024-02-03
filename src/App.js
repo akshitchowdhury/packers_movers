9360b2b42c34465d3c2b2b9a5c2d93df493d9467
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-02-03 12:08:11 +0530

    p5

--- src/App.js
+++ src/App.js
@@ -3,14 +3,49 @@ import './App.css';
 import Nav from './components/Nav/Nav';
 import frame from './components/assets/frame2.jpg'
 import Footer from './components/Footer/Footer';
-
+import { BrowserRouter as Router, Route,Routes, Link, useNavigate } from 'react-router-dom';
+import Home from './components/Home/Home';
 
 
 
 function App() {
+
+  const navigate = useNavigate();
+
+  const handleHomeClick = () => {
+    // Navigate to the default page or refresh the current page
+    navigate('/');
+  };
+
+
   return (
     <div className="App">
+    <div className='navbar'>
     <Nav/>
+<Router>    
+    <div className='links'>
+    <h3 className='home' onClick={handleHomeClick}>HOME</h3>
+    <Link to='/about'> <h3 className='about'>ABOUT US</h3></Link>
+    <Link to='/services'>  <h3 className='services'>SERVICES</h3></Link>
+    <Link to='/gallery'> <h3 className='gallery'>GALLERY</h3></Link>
+    <Link to='/contact'> <h3 className='contact'>CONTACT US</h3></Link>
+    </div>
+
+    <Routes>
+      <Route path='/' element = {<Home/>}/>
+      {/* <Route path='/about' element = {}/>
+      <Route path='/services' element = {}/>
+      <Route path='/gallery' element = {}/>
+      <Route path='/contact' element = {}/> */}
+    </Routes>
+
+
+</Router>
+
+
+
+    </div>
+    
     <div className='frame'>
     <img src={frame}  alt="frame"  />      
     </div>
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,23 +1,18 @@
 import React from 'react'
 import logo from '../assets/logo.png'
 import '../Nav/Nav.css'
+import { Link } from 'react-router-dom';
 const Nav = () => {
   return (
     <>
-    <div className='main'>
+    
     <div className='Nav'>
         <img src= {logo} className='logo'/>
     </div>
     
-    <div className='links'>
-    <h3 className='home'>HOME</h3>
-    <h3 className='about'>ABOUT US</h3>
-    <h3 className='services'>SERVICES</h3>
-    <h3 className='gallery'>GALLERY</h3>
-    <h3 className='contact'>CONTACT US</h3>
-    </div>
+
     
-    </div>
+    
     
     </>
   )
