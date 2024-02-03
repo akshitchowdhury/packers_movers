0dc9fb7aa73a910614e82aaab667a54515e55dbf
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-02-03 09:50:10 +0530

    p3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav/Nav';
+import frame from './components/assets/frame2.jpg'
+import Footer from './components/Footer/Footer';
+
+
+
 
 function App() {
   return (
     <div className="App">
-    <Nav/>      
+    <Nav/>
+    <div className='frame'>
+    <img src={frame}  alt="frame"  />      
+    </div>
+    <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Footer.css'
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <p>
+        Copyright ©2024 Care Packers And Movers. All rights reserved | Designed by Egalitarian Tech Connectivity Pvt. Ltd
+
+        </p>
+    </div>
+  )
+}
+
+export default Footer
