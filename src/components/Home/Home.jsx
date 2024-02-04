4fae77a0ad96605c30c5083f5cec91e256d8d883
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-02-04 14:51:33 +0530

    m6

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -13,12 +13,14 @@ const Home = () => {
   }, []);
 
   return (
+    <>
     <div className="home-container">
       {/* <h1>Welcome to Our Website</h1>
       <p>This is the Home page content. Feel free to customize it according to your needs.</p> */}
     <img src= {frame} alt="frame" />
     
     </div>
+    </>
   );
 }
 
