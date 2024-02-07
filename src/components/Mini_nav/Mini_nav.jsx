d2831ebf65dec59a69a2cac6eedd97c8f78455f9
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-02-07 13:44:18 +0530

    d6

--- src/components/Mini_nav/Mini_nav.jsx
+++ src/components/Mini_nav/Mini_nav.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './Mini_nav.css'
+import { SocialIcon } from 'react-social-icons'
+import { FaFontAwesome } from 'react-icons/fa'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
+
+
+
+
 const Mini_nav = () => {
   return (
         <nav className='mini_nav'>
 
+<FontAwesomeIcon icon="fa-brands fa-facebook" />
+
+         {/* <FontAwesomeIcon icon={faInstagram} />
+      <FontAwesomeIcon icon={faFacebook} /> */}
+        {/* <SocialIcon url="https://twitter.com" /> */}
     <div>Mini_nav</div>
     </nav>
   )
