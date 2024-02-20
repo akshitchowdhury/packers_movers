9157296a7ab237159b3f142661c8008ae67d5ba5
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-02-20 15:36:51 +0530

    navFix

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Logo from '../assets/Logo.jpg';
-
+import './Navbar.css'
 const Navbar = () => {
   return (
     <nav className="bg-amber-300 text-white h-28">
@@ -14,13 +14,13 @@ const Navbar = () => {
         <div className="flex-grow"></div> {/* Fill remaining space */}
         
         <div className="link-tab navbar-links flex flex-col md:flex-row md:items-center md:justify-end w-full md:w-auto md:justify-center">
-          <Link to="/" className="ml-6 text-lg font-medium text-black transition duration-300 hover:text-white hover:text-lg hover:transform hover:scale-110">Home</Link>
-          <Link to="/about" className="ml-6 text-lg font-medium text-black transition duration-300 hover:text-white hover:text-lg hover:transform hover:scale-110">About Us</Link>
-          <Link to="/gallery" className="ml-6 text-lg font-medium text-black transition duration-300 hover:text-white hover:text-lg hover:transform hover:scale-110">Gallery</Link>
-          <Link to="/services" className="ml-6 text-lg font-medium text-black transition duration-300 hover:text-white hover:text-lg hover:transform hover:scale-110">Services</Link>
-          <Link to="/contact" className="ml-6 text-lg font-medium text-black transition duration-300 hover:text-white hover:text-lg hover:transform hover:scale-110">Contact Us</Link>
-        </div>
-      </div>
+  <Link to="/" className="navbar-link ml-6 text-lg md:text-3xl lg:text-3xl xl:text-2xl sm:text-2xl font-medium text-black ">Home</Link>
+  <Link to="/about" className="navbar-link ml-6 text-lg md:text-3xl lg:text-3xl xl:text-2xl sm:text-2xl font-medium text-black ">About Us</Link>
+  <Link to="/gallery" className="navbar-link ml-6 text-lg md:text-3xl lg:text-3xl xl:text-2xl sm:text-2xl font-medium text-black ">Gallery</Link>
+  <Link to="/services" className="navbar-link ml-6 text-lg md:text-3xl lg:text-3xl xl:text-2xl sm:text-2xl font-medium text-black ">Services</Link>
+  <Link to="/contact" className="navbar-link ml-6 text-lg md:text-3xl lg:text-3xl xl:text-2xl sm:text-2xl font-medium text-black ">Contact Us</Link>
+</div>
+</div>
       
     </nav>
   );
--- src/components/Socia_Icons/SocialIcons.jsx
+++ src/components/Socia_Icons/SocialIcons.jsx
@@ -5,10 +5,10 @@ import './SocialIcons.css'; // Import CSS file for styling
 const SocialIcons = () => {
   return (
     <div className="social-icons">
-      <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer" className="youtube-icon"><FaYoutube size={24} /></a>
+      <a href="https://youtube.com/@alwynpackers?si=KYSkjy2uI55jNUzA" target="_blank" rel="noopener noreferrer" className="youtube-icon"><FaYoutube size={24} /></a>
       <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="facebook-icon"><FaFacebook size={24} /></a>
-      <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="instagram-icon"><FaInstagram size={24} /></a>
-      <a href="tel:+123456789" className="phone-icon"><FaPhone size={24} /></a>
+      <a href="https://www.instagram.com/_axe_it_chowdhury/?hl=en" target="_blank" rel="noopener noreferrer" className="instagram-icon"><FaInstagram size={24} /></a>
+      <a href="tel:+918638498584" className="phone-icon"><FaPhone size={24} /></a>
     </div>
   );
 };
